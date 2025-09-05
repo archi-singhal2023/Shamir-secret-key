c126c91908591c8d667b3a91724c7355a9ff755f
Archi <archi.singhal2023@ssipmt.com>
2025-09-05 12:31:20 +0530

    Initial commit

--- app.py
+++ app.py
@@ -21,4 +21,14 @@ def get_combinations(iterable, r):
 def mod_inverse(a, m):
     m0, x0, x1 = m, 0, 1
     while a> 1:
-        
+        q = a // m
+        m, a = a % m, m
+        x0, x1 = x1 - q * x0, x0
+    return x1 + m0 if x1 < 0 else x1
+
+def lagrange_interpolate_at_zer(points, prime):
+    secret = 0
+    k = len(points)
+    for i in range(k):
+        xj, yj = points[i]
+        numerator, denominator = 1, 1
